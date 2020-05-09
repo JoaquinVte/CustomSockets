118386186d61581ae0f250b462183ff5d033e667
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2020-05-09 12:05:59 +0200

    a

--- src/es/ieslavereda/CustomSockets/sockets/DHCPManagerMultiServer.java
+++ src/es/ieslavereda/CustomSockets/sockets/DHCPManagerMultiServer.java
@@ -8,9 +8,26 @@ public class DHCPManagerMultiServer {
 
 	private static int conexionesActivas = 0;
 
+	/**
+	 * Ejecutable para el servidor. Si no se pasa parametro de puerto, este empezara
+	 * a escuchar por el puerto 1977
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
 
 		int portNumber = 1977;
+
+		try {
+			if (args.length > 0)
+				portNumber = Integer.parseInt(args[0]);
+
+		} catch (Exception e) {
+			System.out.println("Parametro no valido, se intenta iniciar por el puerto " + portNumber);
+			portNumber = 1977;
+		}
+
 		boolean listening = true;
 		System.out.println("Socket iniciado en el puerto " + portNumber);
 
